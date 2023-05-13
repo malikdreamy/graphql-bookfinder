688ad6439257df9cb784d609f6baf74b9914b527
malikdreamy <119153047+malikdreamy@users.noreply.github.com>
2023-05-12 19:05:43 -0700

    Update server.js
    update to include Apollo Server typedefs and resolvers and include authMiddleware

--- server/server.js
+++ server/server.js
@@ -1,35 +1,36 @@
-const express = require('express');
+ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const { authMiddleware } = require('./utils/auth');
+ const { authMiddleware } = require('./utils/auth');
 const typeDefs = require('./schemas/typeDefs');
 const resolvers = require('./schemas/resolvers')
 const db = require('./config/connection');
-const routes = require('./routes');
+//const routes = require('./routes');
 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
+  introspection: true,
   typeDefs,
   resolvers,
 
-  //context: authMiddleware,
+context: authMiddleware,
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+//if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/public/index.html'));
 });
 
-//app.use(routes)
+ //app.use(routes)
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
@@ -45,3 +46,4 @@ const startApolloServer = async (typeDefs, resolvers) => {
   
 // Call the async function to start the server
   startApolloServer(typeDefs, resolvers);
+
