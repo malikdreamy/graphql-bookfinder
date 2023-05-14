4be5adbc027902084083ba47b1352174cde5b0b6
malikdreamy <119153047+malikdreamy@users.noreply.github.com>
2023-05-13 22:19:46 -0700

    Update typeDefs.js
    update to error in typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,8 +2,8 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-    _id: ID
-    username: String
+    _id: ID!
+    username: String!
     email: String
     password: String
     bookCount: Int
@@ -11,13 +11,22 @@ const typeDefs = gql`
   }
 
   type Book {
-    id: ID
+    id: ID!
     authors: [String]
     description: String
-    bookId: String
+  
     image: String
     link: String
     title: String
+  }
+
+  input BookInput {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
   }
 
   type Auth {
@@ -26,15 +35,18 @@ const typeDefs = gql`
   }
 
   type Query {
-    me(ID: String!): User
+    me: User
   }
 
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(author: [String!], title: String!, bookId: String!, image: String!, link: String!): User
-    removeBook(bookId: String!): User
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
   }
 `;
 
 module.exports = typeDefs;
+
+
+
