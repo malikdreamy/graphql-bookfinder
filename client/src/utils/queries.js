1fcf7181c01e0abcbcd06523505917c695f26e98
malikdreamy <119153047+malikdreamy@users.noreply.github.com>
2023-05-15 13:04:35 -0700

    Update queries.js
    update to meet query to include savedBooks

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,18 +1,21 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_ME = gql`
-  query me {
+  {
     me {
       _id
       username
       email
-      thoughts {
-        _id
-        thoughtText
-        thoughtAuthor
-        createdAt
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
       }
     }
   }
 `;
+
 
